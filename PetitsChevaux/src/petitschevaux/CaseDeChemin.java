bda3aba86142bc481bf5e1d23ae5685afd081397
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-17 13:51:26 +0200

    Ajout classe CaseDeChemin

---
+++ PetitsChevaux/src/petitschevaux/CaseDeChemin.java
@@ -0,0 +1,20 @@
+package petitschevaux;
+
+public class CaseDeChemin extends Case {
+	
+	public CaseDeChemin(){
+		
+	}
+	
+	public boolean peutPasser(Pion p) {
+		
+		return false; ////////////////////
+	}
+
+	public boolean peutSArreter(Pion p) {
+		
+		return false; ////////////////////
+	}
+	
+
+}
