91b799102f49945d21840587394c6a05fb88d292
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-17 14:23:39 +0200

    Ajout classe JoueurHumain

---
+++ PetitsChevaux/src/petitschevaux/JoueurHumain.java
@@ -0,0 +1,14 @@
+package petitschevaux;
+
+public class JoueurHumain extends Joueur {
+
+	public JoueurHumain(String nom, Couleur c) {
+		super(nom,c);
+	}
+	
+	public Pion choisirPion(int n, Plateau p) {
+		
+		return null;////////////////////
+	}
+	
+}
