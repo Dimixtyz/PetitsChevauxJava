76a24c388d3bb43d46539c66148f7c953aedbe88
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-17 13:55:20 +0200

    Ajout classe CaseDEchelle

---
+++ PetitsChevaux/src/petitschevaux/CaseDEchelle.java
@@ -0,0 +1,20 @@
+package petitschevaux;
+
+public class CaseDEchelle extends CaseColoree {
+	
+	public CaseDEchelle(Couleur c) {
+		super(c);
+		
+	}
+	
+	public boolean peutPasser(Pion p) {
+		
+		return false; ////////////////////////////////////////////
+	}
+	
+	public boolean peutSArreter(Pion p) {
+		
+		return false; //////////////////////////////////////////////
+	}
+
+}
