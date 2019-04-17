735c1d05e7ed6464231b8462fec2b3d296830980
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-17 14:04:22 +0200

    Ajout classe Plateau

---
+++ PetitsChevaux/src/petitschevaux/Plateau.java
@@ -0,0 +1,39 @@
+package petitschevaux;
+
+import java.util.ArrayList;
+
+public class Plateau {
+	
+	private ArrayList<ArrayList<CaseDEchelle>> echelles;
+	
+	public Plateau() {
+		
+	}
+	
+	public ArrayList<CaseEcurie> getEcuries(){
+		
+		return null; //////////////////////////
+	}
+	
+	public ArrayList<ArrayList<CaseDEchelle>> getEchelles(){
+		
+		return echelles;
+	}
+	
+	public ArrayList<CaseDeChemin> getChemin(){
+		
+		return null; //////////////////////////////
+	}
+	
+	public void afficher() {
+		
+		
+	}
+	
+	public void deplacerPionA(Pion p, Case c) {
+		
+		
+		
+	}
+
+}
