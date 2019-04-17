e2f69f2ae5b2a0501d97f45b96500ff73d1187d1
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-17 13:53:24 +0200

    Ajout classe CaseEcurie

---
+++ PetitsChevaux/src/petitschevaux/CaseEcurie.java
@@ -0,0 +1,19 @@
+package petitschevaux;
+
+public class CaseEcurie extends CaseColoree {
+	
+	public CaseEcurie(Couleur c) {
+		super(c);
+	}
+	
+	public boolean peutPasser(Pion p) {
+		
+		return false; ////////////////////////////////////////////
+	}
+	
+	public boolean peutSArreter(Pion p) {
+		
+		return false; //////////////////////////////////////////////
+	}
+
+}
