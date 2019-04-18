627260fc81be3455a5ae42ee079bc52eb6a9891d
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-18 15:11:32 +0200

    Ajout des noms des couleurs

--- PetitsChevaux/src/petitschevaux/Couleur.java
+++ PetitsChevaux/src/petitschevaux/Couleur.java
@@ -2,19 +2,25 @@ package petitschevaux;
 
 public enum Couleur {
 	
-	ROUGE ('R'),
-	JAUNE ('J'),
-	VERT ('V'),
-	BLEU ('B');
+	ROUGE ('R',"Rouge"),
+	JAUNE ('J',"Jaune"),
+	VERT ('V',"Vert"),
+	BLEU ('B',"Bleu");
 	   
 	private char symbol;
+	private String nom;
 	 
 	//Constructeur
-	Couleur(char symbol){
+	Couleur(char symbol, String nom){
 	  this.symbol = symbol;
+	  this.nom = nom;
 	}
 	   
 	public char getSymbol(){
 	  return symbol;
+	}
+	
+	public String getNom() {
+		return nom;
 	}
 }
