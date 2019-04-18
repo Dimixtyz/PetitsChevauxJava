9be448ff3cca08fb11f0ef7af76f45ffca847ba5
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-18 13:44:14 +0200

    implementation classe Joueur

--- PetitsChevaux/src/petitschevaux/Joueur.java
+++ PetitsChevaux/src/petitschevaux/Joueur.java
@@ -1,13 +1,43 @@
 package petitschevaux;
 
-public class Joueur {
+import java.util.ArrayList;
+
+public abstract class Joueur {
 
 	private String nom;
 	private Couleur couleur;
+	private Case caseDeDepart;
+	private ArrayList<Pion> chevaux;
+	
 	
 	public Joueur(String nom, Couleur couleur){
-		
+		this.nom = nom;
+		this.couleur = couleur;
 	}
 	
+	public Case getCaseDeDepart() {
+		return caseDeDepart;
+	}
 	
+	public void setCaseDeDepart(Case c) {
+		this.caseDeDepart = c;
+	}
+	
+	public ArrayList<Pion> getChevaux(){
+		return chevaux;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public Couleur getCouleur() {
+		return this.couleur;
+	}
+	
+	public abstract Pion choisirPion(int n, Plateau p);
 }
