2dfa42dbd7a33faea964435c6a1a922015dfbc18
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-26 10:30:05 +0200

    Javadoc + fonctions

--- PetitsChevaux/src/petitschevaux/CaseEcurie.java
+++ PetitsChevaux/src/petitschevaux/CaseEcurie.java
@@ -6,12 +6,30 @@ public class CaseEcurie extends CaseColoree {
 		super(c);
 	}
 	
+	/**
+	 * Verifie si un pion peut passer case
+	 * @param p un pion
+	 * @return vrai (true) si le pion peut passer la case, faux (false) sinon.
+	 */
 	public boolean peutPasser(Pion p) {
-		return false;
+		/*Un pion peut sortir de l'ecurie seulement si c'est celle de sa couleur*/
+		if(p.getCouleur() == this.getCouleur())
+			return true;
+		else
+			return false;
 	}
 	
+	/**
+	 * Verifie si un pion peut s'arreter sur une case
+	 * @param p un pion
+	 * @return vrai (true) si le pion peut s'arreter sur cette case, faux (false) sinon.
+	 */
 	public boolean peutSArreter(Pion p) {
-		return false;
+		/*Un pion peut s'arreter sur une case ecurie seulement si c'est celle de sa couleur*/
+		if(p.getCouleur() == this.getCouleur())
+			return true;
+		else
+			return false;
 	}
 
 }
