01f5df4e1a7c96422e9db0a4119e70fd10fdd20c
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-26 11:10:36 +0200

    javadoc

--- PetitsChevaux/src/petitschevaux/Pion.java
+++ PetitsChevaux/src/petitschevaux/Pion.java
@@ -5,11 +5,20 @@ public class Pion {
 	private String id;
 	private Couleur couleur;
 	
+	/**
+	 * Constructeur d'un pion
+	 * @param id numero unique pour identifier un pion
+	 * @param c couleur du pion
+	 */
 	public Pion(String id, Couleur c) {
 		this.id = id;
 		this.couleur = c;
 	}
 	
+	/**
+	 * Getter de la couleur d'un pion
+	 * @return retourne la couleur du pion
+	 */
 	public Couleur getCouleur() {
 		return this.couleur;
 	}
