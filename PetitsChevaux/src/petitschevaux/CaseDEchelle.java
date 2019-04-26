1d8d170f4de8aa10358b4385429067b800f95d7a
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-26 10:37:29 +0200

    JavaDoc + fonctions

--- PetitsChevaux/src/petitschevaux/CaseDEchelle.java
+++ PetitsChevaux/src/petitschevaux/CaseDEchelle.java
@@ -7,14 +7,27 @@ public class CaseDEchelle extends CaseColoree {
 		
 	}
 	
+	/**
+	 * Verifie si un pion peut passer une case
+	 * @param p un pion
+	 * @return vrai (true) si le pion peut passer la case, faux (false) sinon.
+	 */
 	public boolean peutPasser(Pion p) {
-		
-		return false; ////////////////////////////////////////////
+		/* On ne peut pas sauter de case echelle */
+		return false;
 	}
 	
+	/**
+	 * Verifie si un pion peut s'arreter sur une case
+	 * @param p un pion
+	 * @return vrai (true) si le pion peut s'arreter sur cette case, faux (false) sinon.
+	 */
 	public boolean peutSArreter(Pion p) {
-		
-		return false; //////////////////////////////////////////////
+		/* S'il n'y a aucun pion le pion peut s'arreter sur la case */
+		if(this.getChevaux() == null)
+			return true;
+		else
+			return false;
 	}
 
 }
