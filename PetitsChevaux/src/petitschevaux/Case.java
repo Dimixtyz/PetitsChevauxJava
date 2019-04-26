c5741eafbd0ea2c85b9dda332da2743b6f171419
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-26 11:05:02 +0200

    Javadoc

--- PetitsChevaux/src/petitschevaux/Case.java
+++ PetitsChevaux/src/petitschevaux/Case.java
@@ -6,24 +6,50 @@ public abstract class Case {
 	
 	private ArrayList<Pion> chevaux = new ArrayList<Pion>();
 	
+	/**
+	 * Constructeur d'une case
+	 */
 	public Case() {
 		
 	}
 	
+	/**
+	 * Getter des pions de la case
+	 * @return la liste des pions sur la case
+	 */
 	public ArrayList<Pion> getChevaux() {
 		return chevaux;
 	}
 	
+	/**
+	 * Fonction pour ajouter un pion sur la case
+	 * @param p le pion a ajouter
+	 */
 	public void ajouteCheval(Pion p) {
 		chevaux.add(p);
 	}
 	
+	/**
+	 * Verifie si un pion peut passer la case
+	 */
 	public abstract boolean peutPasser(Pion p);
 	
+	/**
+	 * Verifie si un pion peut s'arreter sur la case
+	 */
 	public abstract boolean peutSArreter(Pion p);
 	
+	/**
+	 * Fonction pour retirer un cheval de la case
+	 * @param p un pion
+	 */
 	public void retirerCheval(Pion p) {
-		chevaux.remove(p);
+		/*Verifie que le pion est bien present sur la case */
+		for(Pion x : chevaux) {
+			if(x == p)
+				chevaux.remove(p);
+		}
+		
 	}
 
 }
--- PetitsChevaux/src/petitschevaux/CaseColoree.java
+++ PetitsChevaux/src/petitschevaux/CaseColoree.java
@@ -4,16 +4,30 @@ public abstract class CaseColoree extends Case {
 
 	private Couleur couleur;
 	
+	/**
+	 * Constructeur d'une case coloree
+	 * @param c couleur de la case
+	 */
 	public CaseColoree(Couleur c) {
 		this.couleur = c;
 	}
 	
+	/**
+	 * Getter de la couleur de la case
+	 * @return retourne la couleur de la case
+	 */
 	public Couleur getCouleur() {
 		return this.couleur;
 	}
 	
+	/**
+	 * Verifie si un pion peut passer la case
+	 */
 	public abstract boolean peutPasser(Pion p);
 	
+	/**
+	 * Verifie si un pion peut s'arreter sur la case
+	 */
 	public abstract boolean peutSArreter(Pion p);
 	
 }
