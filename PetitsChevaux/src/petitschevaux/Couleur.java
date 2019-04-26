285dc600a9db2ba98ebf33f6bb0e12571cc36905
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-04-26 11:08:27 +0200

    javadoc

--- PetitsChevaux/src/petitschevaux/Couleur.java
+++ PetitsChevaux/src/petitschevaux/Couleur.java
@@ -10,16 +10,28 @@ public enum Couleur {
 	private char symbol;
 	private String nom;
 	 
-	//Constructeur
+	/**
+	 * Constructeur de couleur
+	 * @param symbol premiere lettre de la couleur
+	 * @param nom Nom complet de la couleur
+	 */
 	Couleur(char symbol, String nom){
 	  this.symbol = symbol;
 	  this.nom = nom;
 	}
-	   
+	  
+	/**
+	 * Getter de la premiere lettre de la couleur
+	 * @return retourne la premiere lettre de la couleur
+	 */
 	public char getSymbol(){
 	  return symbol;
 	}
 	
+	/**
+	 * Getter du nom complet de la couleur
+	 * @return retourne le nom complet de la couleur
+	 */
 	public String getNom() {
 		return nom;
 	}
