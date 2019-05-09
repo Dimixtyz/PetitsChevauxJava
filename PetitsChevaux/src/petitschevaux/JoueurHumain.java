90126711cfd1097b222542babf0706b45cc18625
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-05-09 23:54:01 +0200

    Debut fonction choix pion

--- PetitsChevaux/src/petitschevaux/JoueurHumain.java
+++ PetitsChevaux/src/petitschevaux/JoueurHumain.java
@@ -1,5 +1,7 @@
 package petitschevaux;
 
+import java.util.ArrayList;
+
 public class JoueurHumain extends Joueur {
 
 	public JoueurHumain(String nom, Couleur c) {
@@ -7,12 +9,29 @@ public class JoueurHumain extends Joueur {
 	}
 	
 	/**
-	 * Fonction de choix du pion à bouger
+	 * Fonction de choix du pion ï¿½ bouger
 	 * @param valeurDe la valeur du de
 	 * @param p le plateau de la partie
 	 * @return retourne le pion que le joueur joue
 	 */
 	public Pion choisirPion(int valeurDe, Plateau p) {
+		
+		ArrayList<Pion> pionBougable = new ArrayList<Pion>();
+		
+		/*Pion dans l'ecurie*/
+		for(CaseEcurie CE : p.getEcuries()) {
+			
+			if(CE.getCouleur() == this.getCouleur()) {
+				
+				for(Pion pions : CE.getChevaux()){
+					if(valeurDe == 6) {
+						pionBougable.add(pions);
+					}
+				}
+			}
+		}
+		
+		/*Pion sur le chemin*/
 		
 		return null;////////////////////
 	}
