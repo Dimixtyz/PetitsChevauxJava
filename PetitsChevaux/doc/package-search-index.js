99e0aed95c670fc99b64a2e17918cc2b75e7f2cd
Quentin <quentin2000du50@gmail.com>
2019-06-06 21:23:01 +0200

    retrait javaDoc

--- PetitsChevaux/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"fr.cases"},{"l":"fr.exceptionpetitschevaux"},{"l":"fr.jeu"},{"l":"fr.joueur"},{"l":"fr.petitschevaux"}]
